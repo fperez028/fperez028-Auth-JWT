8a7134942fd6a94b84d1abd90bcd4101a7547c0e
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-08-04 18:00:11 +0000

    added login page  and private page routes

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,6 +10,8 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Signup } from "./pages/Signup";
+import { Login } from "./pages/Login";
+import { Private } from "./pages/Private";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -27,6 +29,8 @@ export const router = createBrowserRouter(
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/private" element={<Private />} />
       </Route>
     )
 );
